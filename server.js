4c571379e2049c2d95571a51ec9b745d9690ef18
jchen284 <34975461+jchen284@users.noreply.github.com>
2018-06-03 23:50:53 -0700

    final update

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
+// Seriver js that works for the local hose
 const express = require('express');
 const app = express();
 
 app.use(express.static('static_files'));
-
-const fakeDatabase = {
-  'Chill': {name: 'Chill', locations: 'Library Walk, Sun God Lawn, Revelle Fountain'},
-  'Food': {name: 'Food', locations: 'Taco Villa, 64 North'},
-  'Study': {name: 'Study', locations: 'Biomedical Library, Geisel Library'}
-};
-
-app.get('/:hashtags', (req, res) => {
-  const nameToLookup = req.params.hashtags; // matches any of the hashtags from above
-  const val = fakeDatabase[nameToLookup];
-  console.log(nameToLookup, '->', val); // for debugging
-  if (val) {
-    res.send(val);
-  } else {
-    res.send({}); // failed, so return an empty object instead of undefined
-  }
-});
 
 // start the server at URL: http://localhost:3000/
 app.listen(3000, () => {
